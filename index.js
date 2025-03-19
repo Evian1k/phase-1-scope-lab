b886221d59e9d16b221472ffc96c18c1bd227ce8
Evian1k <emmanuelevian@gmail.com>
2025-03-19 20:39:27 +0300

    Adds

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution in this file!
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";  // No `var`, `let`, or `const` so it becomes global
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";  // Reassigning `bestCustomer`
+}
+
+const returnFirstTwoDrivers= function(drivers) {
+    return drivers.slice(0, 2);
+}
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; // Implicitly global
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; // Reassigns the global variable
+}
+
+const leastFavoriteCustomer = "some customer";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "another customer"; // Should fail
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(array0fDrivers, driverFunction) {
+    return driverFunction(array0fDrivers);
+}
